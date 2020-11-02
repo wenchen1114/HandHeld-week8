a52e9ab3bd3cf1d8e8b41d3c5711b8325f7b58f4
wenchen1114 <kwc354@nyu.edu>
2020-11-02 11:45:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function render(){
     ctx.beginPath();
     ctx.shadowColor = 'rgba(255,103,103,0.05)';
     ctx.shadowBlur = 15;
-    ctx.fillStyle = "rgba(255,103,103,0.01)";
+    ctx.fillStyle = "rgba(255,103,103,0.001)";
     ctx.arc(p3.x, p3.y, 80, 0, Math.PI *2);
     ctx.fill();
 
